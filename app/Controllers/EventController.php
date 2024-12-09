400d68154c7873954abdea07249295d4744bce94
SusanuAlexandru <susanu.alexandru@elev.cihcahul.md>
2024-12-09 08:16:45 +0200

    Exclude fișierele generate automat din repository

---
+++ app/Controllers/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\Event;
+
+class EventController
+{
+    public function index(Request $request, Response $response, $args)
+    {
+        $categories = Event::all();
+        ob_start();
+        require '../views/index.view.php';
+        $html = ob_get_clean();
+        $response->getBody()->write($html);
+        return $response;
+    }
+
+    
+}
